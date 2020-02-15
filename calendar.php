799854f11819768317fa14aa5958dbb77c37d20c
ArtNazarov <33857419+ArtNazarov@users.noreply.github.com>
2020-02-15 20:28:57 +0500

    Raw version

---
+++ calendar.php
@@ -0,0 +1,74 @@
+﻿<?php
+
+
+function weekOfMonth($date) {
+    // estract date parts
+    list($y, $m, $d) = explode('-', date('Y-m-d', strtotime($date)));
+
+    // current week, min 1
+    $w = 1;
+
+    // for each day since the start of the month
+    for ($i = 1; $i <= $d; ++$i) {
+        // if that day was a sunday and is not the first day of month
+        if ($i > 1 && date('w', strtotime("$y-$m-$i")) == 0) {
+            // increment current week
+            ++$w;
+        }
+    }
+
+    // now return
+    return $w;
+}
+
+function getWeekday($date) {
+    return date('w', strtotime($date));
+}
+
+
+function calendar($month, $year){
+
+$days = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+$rows = ceil($days / 7);
+// Init widget
+$calendar = [];
+$index_of_row = 0;
+for($r=0;$r<$rows;$r++){
+	$calendar[$r] = [];
+	for($d=1;$d<8;$d++){
+	$calendar[$r][$d] = ["text"=>""];
+	};
+};
+
+$index_of_row = 0;
+
+for($i=1;$i<$days;$i++){
+
+	$date = "$year-$month-$i";
+	$place = getWeekday($date);
+  $index_of_row=weekOfMonth($date);
+	$calendar[$index_of_row][$place] = ["date" => $date, "text"=>""];
+	};
+
+
+return $calendar;
+};
+
+function output_calendar($month, $year){
+
+  $cal = calendar($month, $year);
+  $html = "<table border='1'>";
+  for ($r=0;$r<count($cal);$r++){
+    $html.="<tr>";
+    for ($d=1;$d<8;$d++){
+      $html.="<td>" . $cal[$r][$d]["date"] . "</td>";
+    };
+    $html .= "</tr>";
+  };
+  $html .= "</table>";
+  return $html;
+}
+
+echo output_calendar(2,2020);
+
+?>
